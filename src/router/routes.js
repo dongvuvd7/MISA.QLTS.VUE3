c312c396c67a23f664e975b460e836f0829985a1
Vũ Đức Đông <78722197+dongvuvd7@users.noreply.github.com>
2022-08-04 15:44:43 +0700

    update authen

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,80 +1,70 @@
-import AssetList from '../view/asset/AssetList.vue'
-import GeneralTemplate from '../view/general/General.vue';
+import AssetList from "../view/asset/AssetList.vue";
+import GeneralTemplate from "../view/general/General.vue";
 import LoginView from "../view/login/LoginView.vue";
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 import store from "../store/store";
 
 const routes = [
     {
-        path: '/',
-        name: 'login',
+        path: "/",
+        name: "login",
         component: LoginView,
-        beforeEnter: (to, from, next) => {
-            if (store.getters.isAuthentication) {
-                next({ name: 'asset' })
-            } else {
-                next()
-            }
-        }
+        meta: { guest: true },
     },
     {
-        path: '/asset',
-        name: 'asset',
+        path: "/asset",
+        name: "asset",
         component: AssetList,
-        beforeEnter: (to, from, next) => {
-            if (store.getters.isAuthentication) {
-                next()
-            } else {
-                next({ name: 'login' })
-            }
-        }
+        meta: { requiresAuth: true },
     },
     {
-        path: '/overview',
-        name: 'overview',
+        path: "/overview",
+        name: "overview",
         component: GeneralTemplate,
-        beforeEnter: (to, from, next) => {
-            if (store.getters.isAuthentication) {
-                next()
-            } else {
-                next({ name: 'login' })
-            }
-        }
+        meta: { requiresAuth: true },
     },
     {
-        path: '/login',
-        name: 'loginPage',
+        path: "/login",
+        name: "loginPage",
         component: LoginView,
-        beforeEnter: (to, from, next) => {
-            if (store.getters.isAuthentication) {
-                next({ name: 'asset' })
-            } else {
-                next()
-            }
-        }
+        meta: { guest: true },
     },
     //Nếu path không tồn tại thì chuyển về login
     {
-        path: '/:pathMatch(.*)*',
-        name: 'unknown',
+        path: "/:pathMatch(.*)*",
+        name: "unknown",
         component: LoginView,
-        beforeEnter: (to, from, next) => {
-            if (store.getters.isAuthentication) {
-                next({ name: 'asset' })
-            } else {
-                next()
-            }
-        }
+        meta: { guest: true },
     },
-
-
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (store.getters.isAuthentication) {
+            next();
+            return;
+        }
+        next("/");
+    } else next();
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.guest)) {
+        if (store.getters.isAuthentication) {
+            next("/asset");
+            return;
+        }
+        next();
+    } else {
+        next();
+    }
 });
 
 export default router;
